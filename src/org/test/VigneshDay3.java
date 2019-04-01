3f3258a1a2529946d62bd1f9f98f543ec8862e0b
VigneshKannadasan <vigneshkannadasan7@gmail.com>
2019-04-01 23:54:28 +0530

    Added new class

---
+++ src/org/test/VigneshDay3.java
@@ -0,0 +1,21 @@
+package org.test;
+
+import java.util.Scanner;
+
+public class VigneshDay3 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the empId");
+		int empId=sc.nextInt();
+		System.out.println("enter the age");
+		int Age=sc.nextInt();
+		if(empId==12||Age==58)
+		{
+			System.out.println("Senior HR");
+		}
+		else {
+			System.out.println("HR");
+
+		}
+	}
+}
